aaab18eefc4c74214dcaa921706126cbc48fd3d3
Ian Jennings <ian@meetjennings.com>
2013-07-11 11:11:31 -0400

    compile android and ios versions

--- platforms/android/bin/Moteio-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/Jennings/Development/mote.io-app/platforms/android/bin/Moteio-debug-unaligned.apk : \
-/Users/Jennings/Development/mote.io-app/platforms/android/bin/Moteio.ap_ \
-/Users/Jennings/Development/mote.io-app/platforms/android/bin/classes.dex \
-/Users/Jennings/Development/mote.io-app/platforms/android/libs/cordova-2.9.0.jar \
